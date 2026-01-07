b1e683fc263abcf149cbd2e4d2acc5bf1a0f5a08
Jose Neto <joseam.neto@live.com>
2026-01-07 19:54:54 +0100

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // This applies the header to all paths in your application
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            // 'self' allows your own domain to iframe the app
+            // Add other domains separated by a space
+            value: "frame-ancestors 'self' https://xmapps.sitecorecloud.io",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
